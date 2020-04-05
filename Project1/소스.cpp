83f07663153ce88b0015ace0d42b83e8e4c3ec0f
jungsoojin <jdyun53658315@gmail.com>
2020-04-05 19:55:01 +0900

    초기 파일 실행

---
+++ Project1/소스.cpp
@@ -0,0 +1,23 @@
+/*
+내용 : 사용자로부터 5개의 정수를 입력받아서, 그 합을 출력하는 프로그램
+날짜 : 2019.02.19
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 0;
+	int result = 0;
+
+	for (int i = 1; i < 6; i++) {
+		cout << i << "번째 정수입력 : ";
+		cin >> num;
+		result += num;
+	}
+
+	cout << "\n합계" << result << endl;
+	return 0;
+	
+}
+
